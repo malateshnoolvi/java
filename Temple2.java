0d2d5877b85534eea5cbb399122d56b3e35e4884
unknown <malatesh.xworkz@gmail.com>
2022-08-04 20:55:56 -0700

    temple

---
+++ Temple2.java
@@ -0,0 +1,27 @@
+class Temple2
+{
+   static void godTemple()
+   {
+	   System.out.println("God Tample");
+   }
+ static void godTemple(String godName)
+   {
+	   System.out.println("god name is ;"+godName);
+   }
+ static void godTemple(int amount)
+   {
+	   System.out.println("ticket amount :"+amount);
+   }
+ static void godTemple(boolean prasad)
+   {
+	   System.out.println("prasad available :"+prasad);
+   }
+ static void godTemple(long moneyCollected)
+   {
+	   System.out.println("money collected in temple :"+moneyCollected);
+   }
+ static void godTemple(double workers)
+   {
+	   System.out.println("number of workers in temple :"+workers);
+   }   
+}
